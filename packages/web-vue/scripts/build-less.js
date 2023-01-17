3e9e4b90db21171bd18cf4bf050516fe559fcead
ChenZhaoYu <790348264@qq.com>
2023-01-17 13:58:34 +0800

    chore: scripts

---
+++ packages/web-vue/scripts/build-less.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { buildStyle } = require('@rd/vue-scripts');
+
+buildStyle({ material: true });
--- packages/web-vue/scripts/build.js
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { buildComponent, buildStyle } = require('@rd/vue-scripts');
-
-buildComponent({ umd: false });
-buildStyle({ material: false });
