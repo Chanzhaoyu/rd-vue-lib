3d9281da53d6163db0ae3443b9e93c041042d432
ChenZhaoYu <790348264@qq.com>
2023-01-17 11:30:14 +0800

    chore: add example

---
+++ packages/web-example/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Example</h1>
+    <Button :disabled="disabled">Button</Button>
+  </div>
+</template>
+
+<script setup>
+import { Button } from '@rd/web-vue'
+</script>
